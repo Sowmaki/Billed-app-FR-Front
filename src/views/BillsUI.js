3dbea3a9928203a0ab86620d3e5f2ea79ddf532a
sowmaki <solene.tassel@hotmail.com>
2024-12-19 14:59:07 +0100

    Bug fixé : Bill.js tri par date

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,7 +20,10 @@ const row = (bill) => {
 }//Ajouter la fonction de téléchargement du PDF?
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  // Trie les bills par date
+  const bills = data.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+  return (bills && bills.length) ? bills.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
